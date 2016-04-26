e2e4f2017a8fce33558dbf22c3a117023d928b5f
Dennis Bunadi <dbunadi@gmail.com>
2016-04-26 16:44:33 +1000

    started maze class, will add funciton prototypes that need to be implemented

---
+++ mazeProject/src/Maze.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class Maze {
+
+	private int[][] maze;
+	private ArrayList<Sentry> sentries;
+	private boolean keyStatus;
+	private int[] start;
+	private int[] end;
+	
+	// add timer
+	
+	
+	
+	
+}
