44cd6ccfd84321c4d21bb8bb3597dc0999b17e44
Irfan A <irfana572@gmail.com>
2016-04-28 06:24:43 +0000

    Added generateMaze() function and tile status values.

--- mazeProject/src/Maze.java
+++ mazeProject/src/Maze.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
-
+//use bytes instead of ints?
 
 public class Maze {
+	//Tile status definitions
+	private static final int EMPTY = 0;
+	private static final int WALL  = 1;
+	private static final int START = 2;
+	private static final int EXIT  = 3;
+
 
 	private int[][] maze;
 	private ArrayList<Sentry> sentries;
@@ -11,7 +17,8 @@ public class Maze {
 	
 	// add timer
 	
-	
-	
+	private void generateMaze() {
+		//TO DO
+	}
 	
 }
