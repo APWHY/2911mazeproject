680c4e6dd5c71be23a5c96aace9c120ed71c818c
Dennis Bunadi <dbunadi@gmail.com>
2016-05-10 09:44:02 +1000

    repo refresh

---
+++ Maze.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+//use bytes instead of ints?
+
+public class Maze {
+	//Tile status definitions
+	private static final int EMPTY = 0;
+	private static final int WALL  = 1;
+	private static final int START = 2;
+	private static final int EXIT  = 3;
+
+
+	private int[][] maze;
+	private ArrayList<Sentry> sentries;
+	private boolean keyStatus;
+	private int[] start;
+	private int[] end;
+	
+	// add timer
+	
+	private void generateMaze() {
+		//TO DO
+	}
+	
+}
