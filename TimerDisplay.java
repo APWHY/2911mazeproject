1a99d48f538fe1711d2ca1debd141fe4e4a3f840
Irfan A <irfana572@gmail.com>
2016-05-24 08:45:34 +0000

    Basic timer.

---
+++ TimerDisplay.java
@@ -0,0 +1,26 @@
+package Default;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.text.JTextComponent;
+
+public class TimerDisplay {
+	private int seconds = 0;
+	
+	public void incrementSecond() {
+		seconds++;
+	}
+	
+	public String getTime(){
+		return hourMinSecFormat(this.seconds);
+	}
+	
+	private String hourMinSecFormat(int seconds) {
+		//Code from: http://stackoverflow.com/questions/6118922/convert-seconds-value-to-hours-minutes-seconds
+		//By: Bigtoes
+		int hours   = seconds / 3600;
+		int minutes = (seconds % 3600) / 60;
+		seconds = seconds % 60;
+		return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+	}
+}
